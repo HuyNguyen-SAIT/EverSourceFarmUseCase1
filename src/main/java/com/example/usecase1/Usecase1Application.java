d52ae0d2a1ea12467dee27ba49f8b3cc08994f79
HuyNguyen-SAIT <Nguyenquanhuy2000@gmail.com>
2020-06-08 11:22:45 -0600

    Initial commit

---
+++ src/main/java/com/example/usecase1/Usecase1Application.java
@@ -0,0 +1,13 @@
+package com.example.usecase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Usecase1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Usecase1Application.class, args);
+    }
+
+}
